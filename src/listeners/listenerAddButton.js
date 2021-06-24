533de3435e1519cab6851dc1801019eceb596a50
OksanaShulha <shushullja@gmail.com>
2021-06-24 18:34:24 +0200

    Fix failing inside listener file

--- src/listeners/listenerAddButton.js
+++ src/listeners/listenerAddButton.js
@@ -2,5 +2,3 @@ import { handlerNewItemList } from "../handlers/handlerNewItemList.js";
 
 const form = document.getElementById("input-form");
 form.addEventListener("click", handlerNewItemList);
-
-

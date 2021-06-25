19f8a0de7f6609982ca77119386b724d4764bc2b
rafael garcia <rago89@gmail.com>
2021-06-25 19:06:02 +0200

    add schema and unit test

--- src/data.js
+++ src/data.js
@@ -1,7 +1,23 @@
-export const state = {
-  itemList: [
-    {
-      check: false,
+import { validate } from "./lib/validate.js";
+
+const schema = {
+  title: "Item list",
+  description: "Shopping list",
+  type: "object",
+  properties: {
+    itemList: {
+      type: "array",
+      description: "list of items to buy",
+      items: {
+        type: "string",
+      },
     },
-  ],
+  },
+  required: ["itemList"],
 };
+
+export const state = {
+  itemList: [],
+};
+
+validate(schema, state);
